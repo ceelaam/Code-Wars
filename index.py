2ec3aa65631b1f6b56a9bc1a554febeeb52e1bec
ceelaam <candice.lam94@gmail.com>
2022-04-17 12:23:24 +0200

    added code war solution for: Alphabet Symmetry

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
-#thing
+#codewarproblem 17.04
+#return the number of words in list that has letters with same index as its respective index in the alphabet
+def solve(arr):
+    alphabet_list = list("abcdefghijklmnopqrstuvwxyz")
+    
+    def compare_index(a):
+        count = 0
+        letter_list = list(a.lower())
+        smaller_list = min([len(alphabet_list),len(letter_list)])
+        for index in range(smaller_list):
+            if letter_list[index] == alphabet_list[index]:
+                count += 1
+        return count
+    
+    return [compare_index(word) for word in arr] if arr else None
+
+#-------------------------------------------------------------------------
