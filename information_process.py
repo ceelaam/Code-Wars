19ee992e52a55e3fd8060bc53c64d6d9462b54b8
ceelaam <candice.lam94@gmail.com>
2022-04-18 11:04:38 +0200

    function that checks if all the odd numbers are greater than the even numbers

--- information_process.py
+++ information_process.py
@@ -20,3 +20,22 @@ def solve(arr):
 def vowel_indices(word):
     vowels = "aeiouyAEIOUY" 
     return [index for index in range(1,(len(word)+1)) if word[index-1] in vowels]
+
+#-----------------------------------------------------------------------------------------
+# return True if every odd number in a list is greater than all the even numbers
+
+def is_odd_heavy(arr):
+    odd_integer_list = [odd for odd in arr if odd % 2 != 0]
+    even_integer_list = [even for even in arr if even % 2 ==0]
+    
+    if odd_integer_list:
+        for odd_num in odd_integer_list:
+            for even_num in even_integer_list:
+                if odd_num < even_num:
+                    return False
+    else:
+        return False
+    
+    return True
+
+#---------------------------------------------------------------------------------------------
